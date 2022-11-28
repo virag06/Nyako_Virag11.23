eb158f80f35446caeffba2a562bebaf2d57ce8c9
Nyakó Virág <nyako.virag@students.jedlik.eu>
2022-11-28 11:03:41 +0100

    3. Menü

--- Functions.py
+++ Functions.py
@@ -1,2 +1,33 @@
 from data import virag,ar,szin
 from os import system
+
+def menu():
+    system('cls')
+    print('----------MENÜ----------')
+    print('0 - Kilépés')
+    print('1 - Virágok')
+    print('2 - Színek')
+    print('3 - Legolcsóbb virág')
+    print('4 - Legdrágább virág')
+    print('5 - Csokor összeállítása')
+    return input('Kérem válasszon:...')
+
+def viragokKiir():
+    system('cls')
+    print('--------VIRÁGOK---------')
+
+def szinekKiir():
+    system('cls')
+    print('---------SZÍNEK---------')
+
+def legolcsobb():
+    system('cls')
+    print('----LEGOLCSÓBB VIRÁG----')
+
+def legdragabb():
+    system('cls')
+    print('----LEGDRÁGÁBB VIRÁG----')
+
+def csokor():
+    system('cls')
+    print('--CSOKOR ÖSSZEÁLLÍTÁSA--')
--- data.py
+++ data.py
@@ -1,3 +1,3 @@
-virag=['Rózsa','Pünkösdi rózsa','Tulipán','Nárcisz','Liliom','Szegfű','Frézia','Gerbera','Dália','Hortenzia','Boglárka','Fátyolvirág','amarillisz']
+virag=['Rózsa','Pünkösdi rózsa','Tulipán','Nárcisz','Liliom','Szegfű','Frézia','Gerbera','Dália','Hortenzia','Boglárka','Fátyolvirág','Amarillisz']
 ar=[1100,1000,450,500,650,350,450,550,600,500,550,250,700]
 szin=['piros','rózsaszín','sárga','sárga','rózsaszín','fehér','lila','sárga','lila','kék','rózsaszín','fehér','piros']
